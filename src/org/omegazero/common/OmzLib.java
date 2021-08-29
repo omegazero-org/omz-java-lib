818ddfcc5b9601ed25ea979d06f45c9e340f5c4b
user94729 <user94729@omegazero.org>
2021-08-29 16:14:38 +0200

    Incremented version number

--- src/org/omegazero/common/OmzLib.java
+++ src/org/omegazero/common/OmzLib.java
@@ -16,7 +16,7 @@ import org.omegazero.common.logging.LoggerUtil;
 
 public class OmzLib {
 
-	public static final String VERSION = "2.3";
+	public static final String VERSION = "2.4";
 
 
 	private static final Logger logger = LoggerUtil.createLogger();

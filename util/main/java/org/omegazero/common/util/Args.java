c9693beaf0c98a1b1cc01779837719591e98872b
user94729 <user94729@omegazero.org>
2022-04-01 12:18:50 +0200

    Args: Made abstract and added DefaultFormat

--- util/main/java/org/omegazero/common/util/Args.java
+++ util/main/java/org/omegazero/common/util/Args.java
@@ -20,7 +20,7 @@ import java.util.Map;
  * 
  * @since 2.1
  */
-public class Args implements Serializable {
+public abstract class Args implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -32,23 +32,14 @@ public class Args implements Serializable {
 
 
 	/**
-	 * Parses the given argument array and stores the data in this {@link Args} object. See {@link #parse(String[])}.
+	 * Parses the given argument array and stores the data in this {@link Args} object.
+	 * <p>
+	 * Values identified as integers, booleans ("<code>true</code>" and "<code>false</code>") or floating-point numbers are automatically converted to the respective type.
 	 * 
 	 * @param args The argument array
+	 * @implNote Use {@link #put(String, String)} to store values
 	 */
-	public void parseArguments(String[] args) {
-		for(int i = 0; i < args.length; i++){
-			String arg = args[i];
-			if(arg.startsWith("--") && i + 1 < args.length){
-				String key = arg.substring(2);
-				put(key, args[i + 1]);
-				i++;
-			}else if(arg.startsWith("-")){
-				String key = arg.substring(1);
-				this.arguments.put(key, true);
-			}
-		}
-	}
+	public abstract void parseArguments(String[] args);
 
 
 	/**
@@ -148,10 +139,17 @@ public class Args implements Serializable {
 	}
 
 
-	private void put(String key, String value) {
+	/**
+	 * Stores the given <b>value</b> identified by the <b>key</b> in this object. The <b>value</b> is automatically converted to the appropriate type, if applicable (for example,
+	 * strings consisting only of digits are converted to a number).
+	 * 
+	 * @param key The key
+	 * @param value The value
+	 */
+	protected void put(String key, String value) {
 		if(value.matches("-?\\d+")){
 			this.arguments.put(key, Long.parseLong(value));
-		}else if(value.matches("-?((\\d+)|(\\d*\\.\\d+)|(\\d+\\.\\d*))")){
+		}else if(value.matches("-?((\\d+)|(\\d*\\.\\d+)|(\\d+\\.\\d*))((e|E)-?\\d+)?")){
 			this.arguments.put(key, Double.parseDouble(value));
 		}else if(value.equals("true")){
 			this.arguments.put(key, true);
@@ -164,27 +162,56 @@ public class Args implements Serializable {
 
 
 	/**
-	 * Parses the given array of arguments in the following format:<br>
-	 * <br>
-	 * Arguments are parsed as key-value pairs. A key-value pair consists of a pair of elements in the given array: the first element is <code>--</code> followed by the key
-	 * string, the second element is the value string. For example:
-	 * 
-	 * <pre>
-	 * --exampleKey value
-	 * </pre>
-	 * 
-	 * The key is "<code>exampleKey</code>" and the value is "<code>value</code>". Values identified as integers, booleans ("<code>true</code>" and "<code>false</code>") or
-	 * floating-point numbers are automatically converted to the respective type.<br>
-	 * If a key string such as "<code>--key</code>" has no value (at the end of the array), it is ignored.<br>
-	 * <br>
-	 * To set a key (for example "<code>key</code>") to the boolean value <code>true</code>, the shorthand "<code>-key</code>" (single dash) may be used.
+	 * Parses the given array of arguments using the default format. See {@link DefaultFormat#parseArguments(String[])}.
 	 * 
 	 * @param args The argument array
 	 * @return The {@link Args} object representing the parsed data
 	 */
 	public static Args parse(String[] args) {
-		Args a = new Args();
+		Args a = new DefaultFormat();
 		a.parseArguments(args);
 		return a;
+	}
+
+
+	/**
+	 * An {@link Args} implementation for the default argument format.
+	 * 
+	 * @since 2.8
+	 */
+	public static class DefaultFormat extends Args {
+
+		private static final long serialVersionUID = 1L;
+
+
+		/**
+		 * {@inheritDoc}
+		 * <p>
+		 * Arguments are parsed as key-value pairs. A key-value pair consists of a pair of elements in the given array: the first element is <code>--</code> followed by the key
+		 * string, the second element is the value string. For example:
+		 * 
+		 * <pre>
+		 * --exampleKey value
+		 * </pre>
+		 * 
+		 * The key is "<code>exampleKey</code>" and the value is "<code>value</code>". If a key string such as "<code>--key</code>" has no value (at the end of the array), it is
+		 * ignored.
+		 * <p>
+		 * To set a key (for example "<code>key</code>") to the boolean value <code>true</code>, the shorthand "<code>-key</code>" (single dash) may be used.
+		 */
+		@Override
+		public void parseArguments(String[] args) {
+			for(int i = 0; i < args.length; i++){
+				String arg = args[i];
+				if(arg.startsWith("--") && i + 1 < args.length){
+					String key = arg.substring(2);
+					this.put(key, args[i + 1]);
+					i++;
+				}else if(arg.startsWith("-")){
+					String key = arg.substring(1);
+					this.arguments.put(key, true);
+				}
+			}
+		}
 	}
 }

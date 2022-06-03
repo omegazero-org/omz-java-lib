64954e5514ade0fb3b0d79c9ad9124676845856c
user94729 <user94729@omegazero.org>
2022-06-03 17:25:40 +0200

    Added AttributeContainer + impl

---
+++ util/main/java/org/omegazero/common/util/AttributeContainer.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2022 omegazero.org, user94729
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package org.omegazero.common.util;
+
+/**
+ * Represents an object that can hold attributes (any {@code Object}), each identified by a unique name string.
+ * 
+ * @since 2.9
+ * @see SimpleAttributeContainer
+ */
+public interface AttributeContainer {
+
+
+	/**
+	 * Retrieves an attachment identified by the given <b>key</b> previously set by {@link #setAttachment(String, Object)}. If no attribute was set with the given <b>key</b>,
+	 * {@code null} is returned.
+	 * 
+	 * @param key The name of the attachment
+	 * @return The value of the attachment, or {@code null} if no attachment with the given <b>key</b> exists
+	 */
+	public Object getAttachment(String key);
+
+	/**
+	 * Stores an object identified by the given <b>key</b>.
+	 * <p>
+	 * Values stored here likely have no meaning in the used application and are purely intended to store metadata used by the application.
+	 * 
+	 * @param key The name string identifying the given <b>value</b> in this {@code AttributeContainer}
+	 * @param value The value to be stored, or {@code null} to delete an existing attachment
+	 */
+	public void setAttachment(String key, Object value);
+
+	/**
+	 * Retrieves an attachment identified by the given <b>key</b> previously set by {@link #setAttachment(String, Object)}. If no attribute was set with the given <b>key</b>, a
+	 * {@code NoSuchElementException} is thrown.
+	 * 
+	 * @param key The name of the attachment
+	 * @return The value of the attachment
+	 * @see #hasAttachment(String)
+	 */
+	public Object requireAttachment(String key);
+
+	/**
+	 * Determines whether an attachment identified by the given <b>key</b> exists.
+	 * 
+	 * @param key The name of the attachment
+	 * @return {@code true} if an attachment with the given <b>key</b> exists
+	 * @see #requireAttachment(String)
+	 */
+	public boolean hasAttachment(String key);
+
+	/**
+	 * Removes an attachment identified by the given <b>key</b> and returns it.
+	 * 
+	 * @param key The name of the attachment
+	 * @return The value of the attachment, or {@code null} if no attachment with the given <b>key</b> existed
+	 */
+	public Object removeAttachment(String key);
+}
---
+++ util/main/java/org/omegazero/common/util/SimpleAttributeContainer.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2022 omegazero.org, user94729
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package org.omegazero.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+/**
+ * An {@link AttributeContainer} implementation using a {@link HashMap}, which may be used as a superclass for more specific objects.
+ * <p>
+ * This class is not thread-safe.
+ * 
+ * @since 2.9
+ */
+public class SimpleAttributeContainer implements AttributeContainer {
+
+
+	/**
+	 * Map containing the values of this {@code SimpleAttributeContainer}.
+	 */
+	protected Map<String, Object> attachments = null;
+
+
+	@Override
+	public Object getAttachment(String key) {
+		if(this.attachments == null)
+			return null;
+		else
+			return this.attachments.get(key);
+	}
+
+	@Override
+	public void setAttachment(String key, Object value) {
+		if(this.attachments == null)
+			this.attachments = new HashMap<>();
+		if(value != null)
+			this.attachments.put(key, value);
+		else
+			this.attachments.remove(key);
+	}
+
+	@Override
+	public Object requireAttachment(String key) {
+		Object v = this.getAttachment(key);
+		if(v != null)
+			return v;
+		else
+			throw new NoSuchElementException("No attribute named '" + key + "'");
+	}
+
+	@Override
+	public boolean hasAttachment(String key) {
+		return this.attachments != null && this.attachments.containsKey(key);
+	}
+
+	@Override
+	public Object removeAttachment(String key) {
+		if(this.attachments == null)
+			return null;
+		else
+			return this.attachments.remove(key);
+	}
+}
